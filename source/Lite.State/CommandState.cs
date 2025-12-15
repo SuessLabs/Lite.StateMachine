b4ad5069e8b1785c2174f949f43c59fbc21a21b2
Damian Suess <djsuess@gmail.com>
2025-12-15 12:58:42 -0500

    Updated method documentation

--- source/Lite.State/CommandState.cs
+++ source/Lite.State/CommandState.cs
@@ -5,15 +5,14 @@ using System;
 
 namespace Lite.State;
 
-/// <summary>
-/// A base class for command states, adding no behavior itself (machine handles timer/subscriptions).
-/// </summary>
+/// <summary>A base class for command states, adding no behavior itself (machine handles timer/subscriptions).</summary>
 public abstract class CommandState<TState> : BaseState<TState>, ICommandState<TState> where TState : struct, Enum
 {
   protected CommandState(TState id) : base(id)
   {
   }
 
+  /// <summary>Message filtration (string objects only).</summary>
   public virtual new Func<object, bool> MessageFilter => _ => true;
 
   public virtual int? TimeoutOverrideMs => null;
