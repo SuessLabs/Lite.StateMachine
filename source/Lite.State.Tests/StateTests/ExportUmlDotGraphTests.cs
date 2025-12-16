7c7e105fdc3d1b2a45415560b3fc1a0628c9a0f0
Damian Suess <djsuess@gmail.com>
2025-12-16 07:16:06 -0500

    ExportUML tests for Error and Failure state transitions

--- source/Lite.State.Tests/StateTests/ExportUmlDotGraphTests.cs
+++ source/Lite.State.Tests/StateTests/ExportUmlDotGraphTests.cs
@@ -1,6 +1,8 @@
 // Copyright Xeno Innovations, Inc. 2025
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Lite.State.Tests.StateTests;
 
 [TestClass]
@@ -22,11 +24,54 @@ public class ExportUmlDotGraphTests
     }
     """;
 
+  private const string ExpectedUmlWithError = """
+    digraph StateMachine {
+      rankdir=LR;
+      compound=true;
+      node [fontname="Segoe UI", fontsize=10];
+      edge [fontname="Segoe UI", fontsize=10];
+      start [shape=point];
+      start -> "State1";
+      "State1" [shape=box];
+      "State2" [shape=box];
+      "State2e" [shape=box];
+      "State3" [shape=doublecircle];
+      "State1" -> "State2" [label="Ok"];
+      "State2" -> "State3" [label="Ok"];
+      "State2" -> "State2e" [label="Error"];
+      "State2e" -> "State2" [label="Ok"];
+    }
+    """;
+
+  private const string ExpectedUmlWithErrorFailure = """
+    digraph StateMachine {
+      rankdir=LR;
+      compound=true;
+      node [fontname="Segoe UI", fontsize=10];
+      edge [fontname="Segoe UI", fontsize=10];
+      start [shape=point];
+      start -> "State1";
+      "State1" [shape=box];
+      "State2" [shape=box];
+      "State2e" [shape=box];
+      "State2f" [shape=box];
+      "State3" [shape=doublecircle];
+      "State1" -> "State2" [label="Ok"];
+      "State2" -> "State3" [label="Ok"];
+      "State2" -> "State2e" [label="Error"];
+      "State2" -> "State2f" [label="Failure"];
+      "State2e" -> "State2" [label="Ok"];
+      "State2f" -> "State1" [label="Ok"];
+    }
+    """;
+
   /// <summary>State definitions.</summary>
   public enum StateId
   {
     State1,
     State2,
+    State2e,
+    State2f,
     State3,
   }
 
@@ -66,6 +111,54 @@ public class ExportUmlDotGraphTests
     AssertExtensions.AreEqualIgnoreLines(ExpectedUml, uml);
   }
 
+  [TestMethod]
+  public void Generates_BasicState_RegisterStateEx_WithError_SuccessTest()
+  {
+    // Assemble
+    var machine = new StateMachine<StateId>()
+      .RegisterStateEx(new StateEx1(StateId.State1), StateId.State2)
+      .RegisterStateEx(
+        state: new StateEx2(StateId.State2),
+        onSuccess: StateId.State3,
+        onError: StateId.State2e)
+      .RegisterStateEx(new StateEx2e(StateId.State2e), StateId.State2)
+      .RegisterStateEx(new StateEx3(StateId.State3))
+      .SetInitialEx(StateId.State1);
+
+    // Act - Generate UML
+    var uml = machine.ExportUml(includeSubmachines: true);
+
+    // Assert
+    Assert.IsNotNull(uml);
+    Console.WriteLine(uml);
+    AssertExtensions.AreEqualIgnoreLines(ExpectedUmlWithError, uml);
+  }
+
+  [TestMethod]
+  public void Generates_BasicState_RegisterStateEx_WithErrorAndFailure_SuccessTest()
+  {
+    // Assemble
+    var machine = new StateMachine<StateId>()
+      .RegisterStateEx(new StateEx1(StateId.State1), StateId.State2)
+      .RegisterStateEx(
+        state: new StateEx2(StateId.State2),
+        onSuccess: StateId.State3,
+        onError: StateId.State2e,
+        onFailure: StateId.State2f)
+      .RegisterStateEx(new StateEx2e(StateId.State2e), StateId.State2)
+      .RegisterStateEx(new StateEx2e(StateId.State2f), StateId.State1)
+      .RegisterStateEx(new StateEx3(StateId.State3))
+      .SetInitialEx(StateId.State1);
+
+    // Act - Generate UML
+    var uml = machine.ExportUml(includeSubmachines: true);
+
+    // Assert
+    Assert.IsNotNull(uml);
+    Console.WriteLine(uml);
+    AssertExtensions.AreEqualIgnoreLines(ExpectedUmlWithErrorFailure, uml);
+  }
+
   #region State Machine - Generic
 
   private class State1 : BaseState<StateId>
@@ -96,6 +189,10 @@ public class ExportUmlDotGraphTests
   private class StateEx1(StateId id) : BaseState<StateId>(id);
 
   private class StateEx2(StateId id) : BaseState<StateId>(id);
+
+  private class StateEx2e(StateId id) : BaseState<StateId>(id);
+
+  private class StateEx2f(StateId id) : BaseState<StateId>(id);
 
   private class StateEx3(StateId id) : BaseState<StateId>(id);
 
