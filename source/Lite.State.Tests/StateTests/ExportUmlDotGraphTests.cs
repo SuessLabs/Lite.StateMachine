cb49be77423b2eb05585cc16a223db7baa1d134e
Damian Suess <djsuess@gmail.com>
2025-12-16 06:54:08 -0500

    ExportUml test of generic RegisterState()

--- source/Lite.State.Tests/StateTests/ExportUmlDotGraphTests.cs
+++ source/Lite.State.Tests/StateTests/ExportUmlDotGraphTests.cs
@@ -1,13 +1,27 @@
 // Copyright Xeno Innovations, Inc. 2025
 // See the LICENSE file in the project root for more information.
 
-using Lite.State.Tests;
-
 namespace Lite.State.Tests.StateTests;
 
 [TestClass]
 public class ExportUmlDotGraphTests
 {
+  private const string ExpectedUml = """
+    digraph StateMachine {
+      rankdir=LR;
+      compound=true;
+      node [fontname="Segoe UI", fontsize=10];
+      edge [fontname="Segoe UI", fontsize=10];
+      start [shape=point];
+      start -> "State1";
+      "State1" [shape=box];
+      "State2" [shape=box];
+      "State3" [shape=doublecircle];
+      "State1" -> "State2" [label="Ok"];
+      "State2" -> "State3" [label="Ok"];
+    }
+    """;
+
   /// <summary>State definitions.</summary>
   public enum StateId
   {
@@ -17,24 +31,26 @@ public class ExportUmlDotGraphTests
   }
 
   [TestMethod]
-  public void Generates_BasicState_SuccessTest()
+  public void Generates_BasicState_RegisterState_SuccessTest()
   {
-    const string ExpectedUml = """
-      digraph StateMachine {
-        rankdir=LR;
-        compound=true;
-        node [fontname="Segoe UI", fontsize=10];
-        edge [fontname="Segoe UI", fontsize=10];
-        start [shape=point];
-        start -> "State1";
-        "State1" [shape=box];
-        "State2" [shape=box];
-        "State3" [shape=doublecircle];
-        "State1" -> "State2" [label="Ok"];
-        "State2" -> "State3" [label="Ok"];
-      }
-      """;
+    // Assemble
+    var machine = new StateMachine<StateId>();
+    machine.RegisterState(new State1());
+    machine.RegisterState(new State2());
+    machine.RegisterState(new State3());
+    machine.SetInitial(StateId.State1);
 
+    // Act - Generate UML
+    var uml = machine.ExportUml(includeSubmachines: true);
+
+    // Assert
+    Assert.IsNotNull(uml);
+    AssertExtensions.AreEqualIgnoreLines(ExpectedUml, uml);
+  }
+
+  [TestMethod]
+  public void Generates_BasicState_RegisterStateEx_SuccessTest()
+  {
     // Assemble
     var machine = new StateMachine<StateId>()
       .RegisterStateEx(new StateEx1(StateId.State1), StateId.State2)
@@ -50,7 +66,7 @@ public class ExportUmlDotGraphTests
     AssertExtensions.AreEqualIgnoreLines(ExpectedUml, uml);
   }
 
-  #region State Machine
+  #region State Machine - Generic
 
   private class State1 : BaseState<StateId>
   {
@@ -73,6 +89,10 @@ public class ExportUmlDotGraphTests
     }
   }
 
+  #endregion State Machine - Generic
+
+  #region State Machine Ex
+
   private class StateEx1(StateId id) : BaseState<StateId>(id)
   {
     public override void OnEnter(Context<StateId> context) => context.NextState(Result.Ok);
@@ -90,5 +110,5 @@ public class ExportUmlDotGraphTests
     }
   }
 
-  #endregion State Machine
+  #endregion State Machine Ex
 }
