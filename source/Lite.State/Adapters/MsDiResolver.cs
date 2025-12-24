11cfaa0746a68a6bc4b95b7e472a9a02717288bb
Damian Suess <djsuess@gmail.com>
2025-12-24 02:21:15 -0500

    Added interface for agnostic Dependency Injection service resolver and MS DI placeholder

---
+++ source/Lite.State/Adapters/MsDiResolver.cs
@@ -0,0 +1,23 @@
+// Copyright Xeno Innovations, Inc. 2025
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lite.State.Adapters;
+
+public sealed class MsDiResolver : IServiceResolver
+{
+  private readonly IServiceProvider _provider;
+
+  public MsDiResolver(IServiceProvider provider) =>
+    _provider = provider;
+
+  public object? InnerContainer => _provider;
+
+  public T CreateInstance<T>()
+    where T : class =>
+    ActivatorUtilities.CreateInstance<T>(_provider);
+
+  public object? GetService(Type serviceType) => _provider.GetService(serviceType);
+}
